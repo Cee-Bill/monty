07631512bcda122570012b806b0aa4b046215cdd
cee-bill <cybillu@gmail.com>
2023-11-15 16:30:36 -0800

    initialization files

---
+++ main.c
@@ -0,0 +1,9 @@
+#include "monty.h"
+
+/* program entry point
+ * open monty file and handle cmd line args
+ * extract the args line by line and tokenize each line
+ * close file
+ * maybe use argc & argv?
+ * no limits on the standrard libraries/functions to be used.
+ */
---
+++ monty.c
@@ -0,0 +1,6 @@
+#include "monty.h"
+
+/* implement push and pall opcode here 
+ * void push(stack_t **stack, unsigned int line_number)
+ * void pall(stack_t **stack, unsigned int line_number)
+ */
