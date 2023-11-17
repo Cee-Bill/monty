897482cc95a663e20573bdc53f5ae6dab35d179a
cee-bill <cybillu@gmail.com>
2023-11-16 17:52:49 -0800

    nop

---
+++ monty_nop.c
@@ -0,0 +1,13 @@
+#include "monty.h"
+
+/**
+  *nop- opcode nop doesn’t do anything
+  *@stack: node struct for stack head
+  *@line_number: line_number
+  *Return: void
+ */
+void nop(stack_t **stack, unsigned int counter)
+{
+	(void) counter;
+	(void) head;
+}
