1aafda6ac58a489a3d3c9d7a68bfe5311e2dae7e
JanefrancessC <janefrancesschioma@gmail.com>
2023-11-18 20:58:45 +0000

    modified main.c

--- main.c
+++ main.c
@@ -1,27 +1,49 @@
 #include "monty.h"
 
-char **tokens;
+var_t var;
 
 /**
  * main - program entry point
  * @argc: arguments count
  * @argv: argument vectors
- * Return: 0 on success
+ * Return: EXIT_SUCCESS if successful
  */
 
 int main(int argc, char **argv)
 {
 	stack_t *stack = NULL;
+	int line_number = 0;
+	FILE *fd;
+	size_t size = 0;
+	char *buffer = NULL;
+	char *line = NULL;
+
+	var.queue = 0;
+	var.stack_length = 0;
 
 	if (argc != 2)
 	{
 		fprintf(stderr, "USAGE: monty file\n");
 		exit(EXIT_FAILURE);
 	}
-	else
+	fd = fopen(argv[1], "r");
+	if (fd == NULL)
 	{
-		read_file(argv[1], &stack);
+		fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
+		exit(EXIT_FAILURE);
 	}
-	free_stack(&stack);
-	return (0);
+	on_exit(free_buffer, &buffer);
+	on_exit(free_stack, &stack);
+	on_exit(fd_close, &fd);
+	while (getline(&buffer, &size, fd) != -1)
+	{
+		line_number++;
+		line = (strtok(buffer, "\n\t\r"));
+		if (line != NULL && line[0] != '#')
+		{
+			process_opcode(line, &stack, line_number);
+		}
+	}
+		exit(EXIT_SUCCESS);
 }
+	 
