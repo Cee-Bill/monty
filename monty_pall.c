8b3cb407845947b0f497a9a7e52ad93eaa91e893
JanefrancessC <janefrancesschioma@gmail.com>
2023-11-18 23:45:19 +0000

    pall op

--- monty_pall.c
+++ monty_pall.c
@@ -1,22 +1,25 @@
 #include "monty.h"
 
-/* monty_pall- opcode pall prints all the values on the stack
- *              starting from the top of the stack. 
+/**
+ * monty_pall- prints all the values on the stack
+ *              starting from the top of the stack.
  * @stack: node data struct for head stack
  * @line_number: file number
  */
 
 void monty_pall(stack_t **stack, unsigned int line_number)
 {
-        (void)line_number;
-        stack_t *curr_stack;
+	(void)line_number;
+	stack_t *curr_stack;
 
-        curr_stack = *stack;
+	curr_stack = *stack;
 
-        while (curr_stack != NULL)
-        {
-                printf("%d\n", curr_stack->n);
-                curr_stack = curr_stack->next;
-        }
-        
+	while (curr_stack != NULL)
+	{
+		printf("%d\n", curr_stack->n);
+		curr_stack = curr_stack->next;
+		if (curr_stack == *stack)
+			return;
+	}
+
 }
---
+++ nodeAdd.c
@@ -0,0 +1,35 @@
+#include "monty.h"
+
+/**
+ * node_add - add a node to the top of the stack
+ * or end of a queue
+ * @stack: Pointer to the array of strings
+ * @n: value to add to node
+ * Return: the new node pointer, NULL otherwise
+ */
+stack_t *node_add(stack_t **stack, const int n)
+{
+	stack_t *new_node;
+
+	if (stack == NULL)
+		return (NULL);
+	new_node = malloc(sizeof(stack_t));
+	if (new_node == NULL)
+		return (NULL);
+	new_node->n = n;
+	if (*stack == NULL)
+	{
+		new_node->prev = new_node;
+		new_node->next = new_node;
+	}
+	else
+	{
+		(*stack)->prev->next = new_node;
+		new_node->prev = (*stack)->prev;
+		(*stack)->prev = new_node;
+		new_node->next = *stack;
+	}
+	if (var.queue == STACK || var.stack_length == 0)
+		*stack = new_node;
+	return (new_node);
+}
